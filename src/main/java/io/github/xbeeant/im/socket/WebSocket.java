a9e8a734c22a6077a5acaf92ea179d76e91fac67
Beeant <huangxb0512@gmail.com>
2023-08-27 11:52:34 +0800

    refactor: 用用户会话当做channel

--- src/main/java/io/github/xbeeant/im/socket/WebSocket.java
+++ src/main/java/io/github/xbeeant/im/socket/WebSocket.java
@@ -6,49 +6,49 @@ import io.github.xbeeant.im.vo.ChatMessage;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.BinaryMessage;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 
 @Component
 @Slf4j
-@ServerEndpoint("/websocket/{groupId}")
+@ServerEndpoint("/websocket/{token}")
 public class WebSocket {
 
     private static final SequenceGenerator sequenceGenerator = new SequenceGenerator(1);
-
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据
-    private Session session;
-    /**
-     * 用户ID
-     */
-    private String groupId;
-
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     //虽然@Component默认是单例模式的，但springboot还是会为每个websocket连接初始化一个bean，所以可以用一个静态set保存起来。
     //  注：底下WebSocket是当前类名
     private static final CopyOnWriteArraySet<WebSocket> webSockets = new CopyOnWriteArraySet<>();
     // 用来存在线连接用户信息
-    private static final ConcurrentHashMap<String, Session> sessionPool = new ConcurrentHashMap<String, Session>();
+    private static final ConcurrentHashMap<String, Map<String, Session>> sessionPool = new ConcurrentHashMap<>();
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
+    /**
+     * 用户会话
+     */
+    private String token;
 
     /**
      * 链接成功调用的方法
      */
     @OnOpen
-    public void onOpen(Session session, @PathParam(value = "groupId") String groupId) {
+    public void onOpen(Session session, @PathParam(value = "token") String token) {
         try {
             this.session = session;
-            this.groupId = groupId;
+            this.token = token;
             webSockets.add(this);
-            sessionPool.put(groupId, session);
+            String uid = tokenToUid(token);
+            Map<String, Session> userSession = sessionPool.get(uid);
+            userSession.put(token, session);
+            sessionPool.put(token, userSession);
             log.info("【websocket消息】有新的连接，总数为:" + webSockets.size());
         } catch (Exception e) {
             log.error("", e);
@@ -62,7 +62,7 @@ public class WebSocket {
     public void onClose() {
         try {
             webSockets.remove(this);
-            sessionPool.remove(this.groupId);
+            sessionPool.remove(this.token);
             log.info("【websocket消息】连接断开，总数为:" + webSockets.size());
         } catch (Exception e) {
             log.error("", e);
@@ -93,7 +93,6 @@ public class WebSocket {
      */
     @OnError
     public void onError(Session session, Throwable error) {
-
         log.error("用户错误,原因:" + error.getMessage());
         log.error("", error);
     }
@@ -102,10 +101,11 @@ public class WebSocket {
     // 此为广播消息
     public void sendAllMessage(ChatMessage message) {
         log.info("【websocket消息】广播消息:" + message);
+        ByteBuffer bf;
         for (WebSocket webSocket : webSockets) {
             try {
                 if (webSocket.session.isOpen()) {
-                    ByteBuffer bf = ByteBuffer.wrap(JSON.toJSONString(message).getBytes(StandardCharsets.UTF_8));
+                    bf = ByteBuffer.wrap(JSON.toJSONString(message).getBytes(StandardCharsets.UTF_8));
                     webSocket.session.getAsyncRemote().sendBinary(bf);
                 }
             } catch (Exception e) {
@@ -114,31 +114,31 @@ public class WebSocket {
         }
     }
 
-    // 此为单点消息
-    public void sendOneMessage(String userId, String message) {
-        Session session = sessionPool.get(userId);
-        if (session != null && session.isOpen()) {
-            try {
-                log.info("【websocket消息】 单点消息:" + message);
-                session.getAsyncRemote().sendText(message);
-            } catch (Exception e) {
-                log.error("", e);
-            }
+    // 此为单点消息(多人)
+    public void sendMoreMessage(String[] tokens, String message) {
+        for (String token : tokens) {
+            sendMessage(message, token);
         }
     }
 
-    // 此为单点消息(多人)
-    public void sendMoreMessage(String[] userIds, String message) {
-        for (String userId : userIds) {
-            Session session = sessionPool.get(userId);
-            if (session != null && session.isOpen()) {
+    // 此为单点消息
+    private void sendMessage(String message, String uid) {
+        Map<String, Session> sessions = sessionPool.get(uid);
+        for (Map.Entry<String, Session> sessionEntry : sessions.entrySet()) {
+            Session value = sessionEntry.getValue();
+            if (value != null && value.isOpen()) {
                 try {
                     log.info("【websocket消息】 单点消息:" + message);
-                    session.getAsyncRemote().sendText(message);
+                    value.getAsyncRemote().sendText(message);
                 } catch (Exception e) {
                     log.error("", e);
                 }
             }
         }
+    }
+
+    private String tokenToUid(String token) {
+        // todo
+        return token;
     }
 }
